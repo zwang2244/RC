88b6b6e2d9810c39d72e94a8e33eaafc6e961866
zwang2244 <43772150+zwang2244@users.noreply.github.com>
2021-05-31 22:07:39 -0500

    Simple rc w/ the statistics(position,speed..)shown

--- new_roller_coaster/rc.js
+++ new_roller_coaster/rc.js
@@ -1,13 +1,14 @@
 /**
  *
  **/
-var Example = Example || {};
+// var Example = Example || {};
 
-Example.rc = function() {
+
     var Engine = Matter.Engine,
         Render = Matter.Render,
         Runner = Matter.Runner,
         Composites = Matter.Composites,
+        Composite = Matter.Composite,
         MouseConstraint = Matter.MouseConstraint,
         Mouse = Matter.Mouse,
         World = Matter.World,
@@ -27,11 +28,17 @@ Example.rc = function() {
     var render = Render.create({
         element: document.body,
         engine: engine,
+        bodyVelocity:true,
         options: {
             width: 1000,
             height: 800,
-            wireframes: false
-        }
+            wireframes: false,
+            showStats: true, //body parts total, body total, constraints total, composites total, collision pairs total
+            //showPerformance: true,
+            showVelocity: true,
+            // showVertexNumbers: true
+            // showPositions: true
+        },
     });
 
     Render.run(render);
@@ -50,19 +57,24 @@ Example.rc = function() {
     ]);
 
     var scale = 0.9;
-    World.add(world, Example.car.car(150, 100, 100 * scale, 40 * scale, 30 * scale));
+    var car1 = Bodies.circle(150,100,20 * scale);
 
-    scale = 0.8;
-    World.add(world, Example.car.car(350, 300, 100 * scale, 40 * scale, 30 * scale));
-    World.add(world, [Bodies.rectangle(200, 200, 100, 50, {friction: 0})]);
+    // Example.car.car(150, 100, 60 * scale, 30 * scale, 20 * scale);
+    World.add(world, car1);
+    //var car2 = Bodies.circle(150,100,20 * scale);
+    //World.add(world, car2);
+    //World.add(world, [Bodies.rectangle(200, 200, 100, 50, {friction: 0})]);
+    //World.add(world, Example.stats(render, render.context, render.time));
     // var CUSTOM_PATH = '425.6 327 273.8';
     World.add(world, [
-        Bodies.rectangle(200, 150, 400, 20, { isStatic: true, angle: Math.PI * 0.09 }),
+        // Bodies.rectangle(200, 150, 400, 20, { isStatic: true, angle: Math.PI * 0.09 }),
         // Bodies.rectangle(500, 350, 650, 20, { isStatic: true, angle: -Math.PI * 0.06 }),
         // Bodies.rectangle(300, 560, 600, 20, { isStatic: true, angle: Math.PI * 0.04 })
 
         // Bodies.fromVertices(300, 300,  Matter.Vertices.fromPath(CUSTOM_PATH))
     ]);
+
+    console.log(car1.x);
 
     //ADD TRACK
     // add bodies
@@ -77,29 +89,25 @@ Example.rc = function() {
                 .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
         };
 
-        ([
-            './svg/c1.svg',
-            // './svg/iconmonstr-paperclip-2-icon.svg',
-            // './svg/iconmonstr-puzzle-icon.svg',
-            // './svg/iconmonstr-user-icon.svg'
-        ]).forEach(function(path, i) {
-            loadSvg(path).then(function(root) {
-                var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
 
-                var vertexSets = select(root, 'path')
-                    .map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 30), 0.1, 0.1); });
+        //load c1 curve
+        loadSvg('./svg/c1.svg').then(function(root) {
+            var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
 
-                World.add(world, Bodies.fromVertices(200 + i * 150, 500 + i * 50, vertexSets, {
-                    render: {
-                        fillStyle: color,
-                        strokeStyle: color,
-                        lineWidth: 1
-                    },
-                    isStatic: true
-                }, true));
-            });
+            var vertexSets = select(root, 'path')
+                .map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 30), 0.1, 0.1); });
+
+            World.add(world, Bodies.fromVertices(200, 500, vertexSets, {
+                render: {
+                    fillStyle: color,
+                    strokeStyle: color,
+                    lineWidth: 1
+                },
+                isStatic: true
+            }, true));
         });
 
+        //load c4 curve
         loadSvg('./svg/c4.svg').then(function(root) {
             var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
 
@@ -143,18 +151,18 @@ Example.rc = function() {
         max: { x: 800, y: 600 }
     });
 
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        render: render,
-        canvas: render.canvas,
-        stop: function() {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
-    };
-};
+    // // context for MatterTools.Demo
+    // return {
+    //     engine: engine,
+    //     runner: runner,
+    //     render: render,
+    //     canvas: render.canvas,
+    //     stop: function() {
+    //         Matter.Render.stop(render);
+    //         Matter.Runner.stop(runner);
+    //     }
+    // };
+
 
 
 /**
@@ -223,37 +231,86 @@ Example.car.car = function(xx, yy, width, height, wheelSize) {
         length: 0
     });
 
-    Composite.addBody(car, body);
+    // Composite.addBody(car, body);
     Composite.addBody(car, wheelA);
-    Composite.addBody(car, wheelB);
-    Composite.addConstraint(car, axelA);
-    Composite.addConstraint(car, axelB);
+    // Composite.addBody(car, wheelB);
+    //Composite.addConstraint(car, axelB);
 
     return car;
 };
 
 
+Render.stats = function(render, context, time) {
+    var engine = render.engine,
+        world = engine.world,
+        bodies = Composite.allBodies(world),
+        parts = 0,
+        width = 80,
+        height = 44,
+        x = 0,
+        y = 0;
+
+    // count parts
+    for (var i = 0; i < bodies.length; i += 1) {
+        parts += bodies[i].parts.length;
+    }
+
+    //console.log(bodies[4].speed);
+    // sections
+    var sections = {
+        'Car': 'Car 1',
+        'Position x': bodies[4].position.x.toFixed(2),
+        'Position y': bodies[4].position.y.toFixed(2),
+        'Velocity x': bodies[4].velocity.x.toFixed(2),
+        'Velocity y': bodies[4].velocity.y.toFixed(2),
+        'Speed': bodies[4].speed.toFixed(2),
+        'Friction': bodies[4].friction.toFixed(2),
+    };
+
+    // background
+    context.fillStyle = '#0e0f19';
+    context.fillRect(x, y, width * 5.5, height);
+
+    context.font = '15px Arial';
+    context.textBaseline = 'top';
+    context.textAlign = 'right';
+
+    // sections
+    for (var key in sections) {
+        var section = sections[key];
+        // label
+        context.fillStyle = '#aaa';
+        context.fillText(key, x + width, y + 8);
+
+        // value
+        context.fillStyle = '#eee';
+        context.fillText(section, x + width, y + 26);
+
+        x += width;
+    }
+};
+
 // create demo interface
 // not required to use Matter.js
 
-MatterTools.Demo.create({
-    toolbar: {
-        title: 'ROLLER COASTER V1',
-        // url: 'https://github.com/liabru/matter-js',
-        reset: true,
-        source: true,
-        fullscreen: true,
-        exampleSelect: true
-    },
-    preventZoom: true,
-    // resetOnOrientation: true,
-    examples: [
-        {
-            name: 'Roller_Coaster',
-            id: 'rc',
-            init: Example.rc
-        }
-    ]
-});
+// MatterTools.Demo.create({
+//     toolbar: {
+//         title: 'ROLLER COASTER V1',
+//         // url: 'https://github.com/liabru/matter-js',
+//         reset: true,
+//         source: true,
+//         fullscreen: true,
+//         exampleSelect: true
+//     },
+//     preventZoom: true,
+//     // resetOnOrientation: true,
+//     examples: [
+//         {
+//             name: 'Roller_Coaster',
+//             id: 'rc',
+//             init: Example.rc
+//         }
+//     ]
+// });
 
 
